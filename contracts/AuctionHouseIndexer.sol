9ca092b1de44fdd56f622a890cf861ee10681095
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-08-03 11:01:29 -0400

    AuctionHouseIndexer will now be able to track Trusted Auctions

--- contracts/AuctionHouseIndexer.sol
+++ contracts/AuctionHouseIndexer.sol
@@ -9,6 +9,14 @@ import "./AuctionHouse.sol";
 contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     AuctionHouse private _auctionHouseContract;
     address _auctionHouseAddress;
+    uint16 _maxPaginationSize;
+    //////////////////////////////////////////////////////////////////////////
+    // Total Trusted token auctions
+    uint public _totalTrustedTokenAuctions;
+    // Holds the ids of the trusted Auctions ( index => auctionId )
+    mapping(uint => uint) _trustedTokenAuctions;
+    // Help us know if a specific token address was marked as trusted or not.
+    mapping(address => bool) _tokenIsTrusted;
     //////////////////////////////////////////////////////////////////////////
     // Total auctions made for a specific token
     mapping(address => uint) public _totalTokenAuctions;
@@ -37,8 +45,8 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
     // Public Views                                                 ////
     ////////////////////////////////////////////////////////////////////
     function getAuctionIdsCreatedByAddress(address creatorAddress, uint[] memory indexes) external view returns(uint[] memory) {
-        // GA: You can request 30 ids at once
-        require(indexes.length <= 30, "GA");
+        // PAG: Exceeds pagination limit
+        require(indexes.length <= _maxPaginationSize, "PAG");
         uint[] memory auctionIds = new uint[](indexes.length);
         for(uint i = 0; i < indexes.length; i++) {
             auctionIds[i] = _userAuctions[creatorAddress][indexes[i]];
@@ -47,8 +55,8 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
     }
 
     function getAuctionIdsParticipatedByAddress(address participatingAddress, uint[] memory indexes) external view returns(uint[] memory) {
-        // GA: You can request 30 ids at once
-        require(indexes.length <= 30, "GA");
+        // PAG: Exceeds pagination limit
+        require(indexes.length <= _maxPaginationSize, "PAG");
         uint[] memory auctionIds = new uint[](indexes.length);
         for(uint i = 0; i < indexes.length; i++) {
             auctionIds[i] = _userParticipatedAuctions[participatingAddress][indexes[i]];
@@ -57,8 +65,8 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
     }
 
     function getAuctionIdsByAuctionedTokenAddress(address auctionedTokenAddress, uint[] memory indexes) external view returns(uint[] memory) {
-        // GA: You can request 30 ids at once
-        require(indexes.length <= 30, "GA");
+        // PAG: Exceeds pagination limit
+        require(indexes.length <= _maxPaginationSize, "PAG");
         uint[] memory auctionIds = new uint[](indexes.length);
         for(uint i = 0; i < indexes.length; i++) {
             auctionIds[i] = _tokenAuctions[auctionedTokenAddress][indexes[i]];
@@ -67,11 +75,21 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
     }
 
     function getAuctionIdsPaidWithTokenAddress(address paidWithTokenAddress, uint[] memory indexes) external view returns(uint[] memory) {
-        // GA: You can request 30 ids at once
-        require(indexes.length <= 30, "GA");
+        // PAG: Exceeds pagination limit
+        require(indexes.length <= _maxPaginationSize, "PAG");
         uint[] memory auctionIds = new uint[](indexes.length);
         for(uint i = 0; i < indexes.length; i++) {
             auctionIds[i] = _paidWithTokenAuctions[paidWithTokenAddress][indexes[i]];
+        }
+        return auctionIds;
+    }
+
+    function getTrustedAuctionIds(uint[] memory indexes) external view returns(uint[] memory) {
+        // PAG: Exceeds pagination limit
+        require(indexes.length <= _maxPaginationSize, "PAG");
+        uint[] memory auctionIds = new uint[](indexes.length);
+        for(uint i = 0; i < indexes.length; i++) {
+            auctionIds[i] = _trustedTokenAuctions[indexes[i]];
         }
         return auctionIds;
     }
@@ -94,6 +112,11 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
         // Register the AuctionIndex for the User
         _userTotalAuctions[creatorAddress] += 1;
         _userAuctions[creatorAddress][_userTotalAuctions[creatorAddress]] = auctionId;
+        // If both tokens are trusted, this Auction will be stored on the list of trusted Auctions
+        if(_tokenIsTrusted[auctionedTokenAddress] && _tokenIsTrusted[paidWithTokenAddress]) {
+            _totalTrustedTokenAuctions += 1;
+            _trustedTokenAuctions[_totalTrustedTokenAuctions] = auctionId;
+        }
         return true;
     }
 
@@ -105,20 +128,30 @@ contract AuctionHouseIndexer is Initializable, UUPSUpgradeable, OwnableUpgradeab
         }
         return true;
     }
+
     ////////////////////////////////////////////////////////////////////
     // Only Owner                                                   ////
     ////////////////////////////////////////////////////////////////////
     function initialize() initializer public {
         __Ownable_init();
+        _maxPaginationSize = 30;
+        // By default the only trusted token will be the Network currency
+        _tokenIsTrusted[address(0)] = true;
     }
+
     function _authorizeUpgrade(address) internal override onlyOwner {}
     function _setAuctionHouseContract(address contractAddress) external onlyOwner {
         _auctionHouseAddress = contractAddress;
         _auctionHouseContract = AuctionHouse(contractAddress);
         emit AuctionHouseContractUpdated(contractAddress);
     }
+    function _updateTokenTrust(address tokenAddress, bool isTrusted) external onlyOwner {
+        _tokenIsTrusted[tokenAddress] = isTrusted;
+        emit UpdatedTokenTrust(tokenAddress, isTrusted);
+    }
     ////////////////////////////////////////////////////////////////////
     // Events                                                       ////
     ////////////////////////////////////////////////////////////////////
+    event UpdatedTokenTrust(address tokenAddress, bool isTrusted);
     event AuctionHouseContractUpdated(address newContractAddress);
 }
