dd19b26ed36738d75066d867880eb1932cade2a3
Felipevji <86337891+Felipevji@users.noreply.github.com>
2021-08-14 16:49:35 -0400

    Auction house tests

---
+++ test/auctionHouse/getAuctions.ts
@@ -0,0 +1,4 @@
+describe('[Auction House] function getAuctions', async () => {
+    it('Should Limit the amount of auctions seen at 30 max', async () => {});
+    it('Should return the auctions with msg.sender current bid', async () => {});
+});
