7835cf6587916bcb47834d5fb5d7f4785e98909f
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-08-16 19:50:13 -0400

    Greed Starter _setTreasuryAddressAndFees test

--- test/greedStarter/@greedStarterTests.ts
+++ test/greedStarter/@greedStarterTests.ts
@@ -3,10 +3,12 @@ import {createProject} from "./createProject.test";
 import {invest} from "./invest.test";
 import {claimFundsTest} from "./claimFunds.test";
 import {_setIndexer} from "./_setIndexer.test";
+import {_setTreasuryAddressAndFees} from "./_setTreasuryAddressAndFees";
 
 export function greedStarterTests() {
     describe('initialize', initialize);
     describe('_setIndexer', _setIndexer);
+    describe('_setTreasuryAddressAndFees', _setTreasuryAddressAndFees);
     describe('createProject', createProject);
     describe('invest', invest);
     describe('claimFunds', claimFundsTest);
