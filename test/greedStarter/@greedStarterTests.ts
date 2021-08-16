630ae3ab2816e4eab3cffc4b57009b7fc0338e4d
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-08-16 19:57:59 -0400

    Greed Starter _setMinimumProjectLength test

--- test/greedStarter/@greedStarterTests.ts
+++ test/greedStarter/@greedStarterTests.ts
@@ -4,11 +4,13 @@ import {invest} from "./invest.test";
 import {claimFundsTest} from "./claimFunds.test";
 import {_setIndexer} from "./_setIndexer.test";
 import {_setTreasuryAddressAndFees} from "./_setTreasuryAddressAndFees";
+import {_setMinimumProjectLength} from "./_setMinimumProjectLength.test";
 
 export function greedStarterTests() {
     describe('initialize', initialize);
     describe('_setIndexer', _setIndexer);
     describe('_setTreasuryAddressAndFees', _setTreasuryAddressAndFees);
+    describe('_setMinimumProjectLength', _setMinimumProjectLength);
     describe('createProject', createProject);
     describe('invest', invest);
     describe('claimFunds', claimFundsTest);
