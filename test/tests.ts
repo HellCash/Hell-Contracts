5a113bbe6a7791b832a89b101329f310fd3f12dd
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-08-17 12:37:34 -0400

    Update tests.ts

--- test/tests.ts
+++ test/tests.ts
@@ -1,7 +1,11 @@
 import {auctionHouseTests} from "./auctionHouse/@auctionHouseTests";
 import {greedStarterTests} from "./greedStarter/@greedStarterTests";
 import {hellTests} from "./hell/@hellTests";
+import {auctionHouseIndexerTests} from "./auctionHouseIndexer/@auctionHouseIndexerTests";
+import {greedStarterIndexerTests} from "./greedStarterIndexer/@greedStarterIndexerTests";
 
-describe('[Auction House]', auctionHouseTests);
-describe('[Greed Starter]', greedStarterTests);
 describe('[Hell]', hellTests);
+describe('[Auction House]', auctionHouseTests);
+describe('[Auction House Indexer]', auctionHouseIndexerTests);
+describe('[Greed Starter]', greedStarterTests);
+describe('[Greed Starter Indexer]', greedStarterIndexerTests);
