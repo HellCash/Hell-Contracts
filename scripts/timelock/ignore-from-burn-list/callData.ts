ceb7910ca0eb241bbf5f28b62f640596260715e3
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-03 15:34:42 -0400

    Ignore from burnlist timelock script

--- scripts/timelock/ignore-from-burn-list/callData.ts
+++ scripts/timelock/ignore-from-burn-list/callData.ts
@@ -6,5 +6,5 @@ export async function callData() {
     const hellContract: Contract = await HellTestHelpers.getHellContract();
     return hellContract.interface.encodeFunctionData(
         '_setExcludedFromBurnList',
-        ['0xC5B3F5a3431Ab036B422f860e2e82234aa76C4b6', true]);
+        ['0xC17773BcE74E471e0eeaEae8fb5888d63b52Fa81', true]);
 }
--- scripts/timelock/ignore-from-burn-list/execute.ts
+++ scripts/timelock/ignore-from-burn-list/execute.ts
@@ -1,24 +1,42 @@
 import {ethers} from "hardhat";
-import {AuctionTestHelpers} from "../../../helpers/AuctionTestHelpers";
 import {TimelockControllerHelpers} from "../../../helpers/TimelockControllerHelpers";
 import {Contract} from "ethers";
 import {txConfirmation} from "../../../utils/network-utils";
 import {zeroBytes32} from "../../../utils/ether-utils";
 import { callData } from "./callData";
 import {HellTestHelpers} from "../../../helpers/HellTestHelpers";
+import {ErrorHandler} from "../../../utils/error-handler";
 
 async function execute() {
     const masterSigner = (await ethers.getSigners())[0];
     const hellContract: Contract = await HellTestHelpers.getHellContract(masterSigner);
     const timelockContract: Contract = await TimelockControllerHelpers.getTimelockControllerContract(masterSigner);
 
-    await txConfirmation('Confirm timelock execute', timelockContract.execute(
-        hellContract.address, // Target
-        0, // Ether
-        await callData(),
-        zeroBytes32, // Predecessor
-        zeroBytes32, // Salt
-    ));
+    try {
+        await txConfirmation('Confirm timelock execute', timelockContract.execute(
+            hellContract.address, // Target
+            0, // Ether
+            await callData(),
+            zeroBytes32, // Predecessor
+            zeroBytes32, // Salt
+        ));
+    } catch (e) {
+        console.log(ErrorHandler.getMessage(e));
+        const id = await timelockContract.hashOperation(
+            hellContract.address,
+            0,
+            await callData(),
+            zeroBytes32,
+            zeroBytes32
+        );
+        console.log('\tMin Delay = ' + await timelockContract.getMinDelay());
+        console.log('\tTimestamp = ' + await timelockContract.getTimestamp(id));
+        console.log('\tIs Operation = ' + await timelockContract.isOperation(id));
+        console.log('\tIs Pending = ' + await timelockContract.isOperationPending(id));
+        console.log('\tIs Ready = ' + await timelockContract.isOperationReady(id));
+        console.log('\tIs Done = ' + await timelockContract.isOperationDone(id));
+        console.log('\tTimelock is the Owner = ' + await timelockContract.address == await hellContract.owner());
+    }
 }
 
 execute()
