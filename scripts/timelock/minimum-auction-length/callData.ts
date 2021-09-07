dcea4f4b4ccd3b2d891aba9b6273f77c89df3a24
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-07 17:51:31 -0300

    Removed unnecessary files

--- scripts/timelock/minimum-auction-length/callData.ts
+++
@@ -1,9 +0,0 @@
-import {BigNumber, Contract} from "ethers";
-import {AuctionTestHelpers} from "../../../helpers/AuctionTestHelpers";
-
-export async function callData() {
-    const auctionContract: Contract = await AuctionTestHelpers.getAuctionContract();
-    return auctionContract.interface.encodeFunctionData(
-        '_setMinimumAuctionLength',
-        [BigNumber.from(350)]);
-}
