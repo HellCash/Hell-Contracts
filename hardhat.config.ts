856a0342fde358b501e73533923cab817aedc80c
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-07 18:14:41 -0300

    Default hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,34 +2,16 @@ import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import 'hardhat-contract-sizer';
 import 'hardhat-deploy';
-import "hardhat-erc1820"; // Hardhat plugin to automatically deploy the ERC-1820 Registry contract.
 
 export default {
   solidity: "0.8.7",
-  defaultNetwork: "localhost",
+  defaultNetwork: "hardhat",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      accounts: [],
-    },
     hardhat: {
       accounts: {
         count: 1000
       }
     },
-    bsc_testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      gasLimit: 100000000,
-      accounts: [],
-    },
-    bsc_mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      // accounts: {mnemonic: mnemonic}
-    }
   },
   settings: {
     optimizer: {
