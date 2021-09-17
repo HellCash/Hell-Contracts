c51286c358da5356368bc8c9efebe4ba37234265
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-17 16:33:35 -0300

    Update @hellGovernmentTests.ts

--- test/hellGovernment/@hellGovernmentTests.ts
+++ test/hellGovernment/@hellGovernmentTests.ts
@@ -1,5 +1,9 @@
 import {initialize} from "./initialize.test";
+import {upgradeTo} from "./upgradeTo";
+import {upgradeToAndCall} from "./upgradeToAndCall";
 
 export function hellGovernmentTests() {
     describe('function initialize', initialize);
+    describe('function upgradeTo', upgradeTo);
+    describe('function upgradeToAndCall', upgradeToAndCall);
 }
