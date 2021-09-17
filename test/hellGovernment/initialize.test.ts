3afb63c02a4a395cea0f314d6ddec02b7be0ec22
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-17 13:57:58 -0300

    Hell Government test initialize

---
+++ test/hellGovernment/initialize.test.ts
@@ -0,0 +1,24 @@
+import {expect} from "chai";
+import {BigNumber} from "ethers";
+import {hellGovernmentTestingEnvironment} from "./@hellGovernmentTestingEnvironment";
+
+export function initialize() {
+    let environment: hellGovernmentTestingEnvironment = new hellGovernmentTestingEnvironment();
+    before(async () => {
+        await environment.initialize();
+    });
+
+    it('should already be initialized', async() => {
+        await expect(environment.hellGovernmentContract.connect(environment.guest1Signer)
+            .initialize(
+                environment.treasurySigner.address,
+                1,
+                BigNumber.from(1),
+                BigNumber.from(1),
+                1,
+                BigNumber.from(1),
+                BigNumber.from(1),
+            ))
+            .to.be.revertedWith("Initializable: contract is already initialized");
+    });
+}
