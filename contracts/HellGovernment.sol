ee60416932dc7adac426c163641b42ef5edc3a54
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-17 12:44:08 -0300

    HellGovernment and HellGoverned Contracts

---
+++ contracts/HellGovernment.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: BSD 3-Clause
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract HellGovernment is Initializable, UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    // General variables
+    address payable public _hellTreasuryAddress;
+    uint16 public _generalPaginationLimit;
+    // Help us know if a specific token address was marked as trusted or not.
+    mapping(address => bool) public _tokenIsTrusted;
+    // Auction House variables
+    uint16 public _auctionHouseTreasuryFee;
+    uint public _minimumAuctionLength;
+    uint public _maximumAuctionLength;
+    // Greed Starter variables
+    uint16 public _greedStarterTreasuryFee;
+    uint public _minimumProjectLength;
+    uint public _maximumProjectLength;
+    ////////////////////////////////////////////////////////////////////
+    // Only Owner                                                   ////
+    ////////////////////////////////////////////////////////////////////
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+    /////////////////////////////////////
+    // General                      ////
+    ////////////////////////////////////
+    function initialize(address payable treasuryAddress, uint16 auctionHouseFee, uint minimumAuctionLength, uint maximumAuctionLength, uint16 greedStarterFee, uint minimumProjectLength, uint maximumProjectLength) initializer public {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        _generalPaginationLimit = 40;
+        _setTreasuryAddress(treasuryAddress);
+        // Initialize Auction House Variables
+        _setAuctionHouseTreasuryFees(auctionHouseFee);
+        _setMinimumAndMaximumAuctionLength(minimumAuctionLength, maximumAuctionLength);
+        // Initialize Greed Starter Variables
+        _setGreedStarterTreasuryFees(greedStarterFee);
+        _setMinimumAndMaximumProjectLength(minimumProjectLength, maximumProjectLength);
+        // By default the only trusted token will be the Network currency
+        _tokenIsTrusted[address(0)] = true;
+    }
+
+    function _setTreasuryAddress(address payable treasuryAddress) public onlyOwner {
+        _hellTreasuryAddress = treasuryAddress;
+        emit TreasuryAddressUpdated(treasuryAddress);
+    }
+
+    function _setGeneralPaginationLimit(uint16 newPaginationLimit) public onlyOwner {
+        _generalPaginationLimit = newPaginationLimit;
+        emit GeneralPaginationLimitUpdated(newPaginationLimit);
+    }
+
+    function _setTokenTrust(address tokenAddress, bool isTrusted) external onlyOwner {
+        _tokenIsTrusted[tokenAddress] = isTrusted;
+        emit UpdatedTokenTrust(tokenAddress, isTrusted);
+    }
+    /////////////////////////////////////
+    // Auction House                ////
+    ////////////////////////////////////
+    function _setMinimumAndMaximumAuctionLength(uint newMinimumLength, uint newMaximumLength) public onlyOwner {
+        _minimumAuctionLength = newMinimumLength;
+        _maximumAuctionLength = newMaximumLength;
+        emit MinimumAndMaximumAuctionLengthUpdated(newMinimumLength, newMaximumLength);
+    }
+
+    function _setAuctionHouseTreasuryFees(uint16 newFee) public onlyOwner {
+        _auctionHouseTreasuryFee = newFee;
+        emit AuctionHouseTreasuryFeesUpdated(newFee);
+    }
+    /////////////////////////////////////
+    // Greed Starter                ////
+    ////////////////////////////////////
+    function _setMinimumAndMaximumProjectLength(uint newMinimumLength, uint newMaximumLength) public onlyOwner {
+        _minimumProjectLength = newMinimumLength;
+        _maximumProjectLength = newMaximumLength;
+        emit MinimumAndMaximumProjectLengthUpdated(newMinimumLength, newMaximumLength);
+    }
+
+    function _setGreedStarterTreasuryFees(uint16 newFee) public onlyOwner {
+        _greedStarterTreasuryFee = newFee;
+        emit GreedStarterTreasuryFeesUpdated(newFee);
+    }
+    ////////////////////////////////////////////////////////////////////
+    // Events                                                       ////
+    ////////////////////////////////////////////////////////////////////
+    event TreasuryAddressUpdated(address indexed treasuryAddress);
+    event GeneralPaginationLimitUpdated(uint16 newLimit);
+    event UpdatedTokenTrust(address tokenAddress, bool isTrusted);
+    // Auction House
+    event AuctionHouseTreasuryFeesUpdated(uint16 newFee);
+    event MinimumAndMaximumAuctionLengthUpdated(uint newMinimumLength, uint newMaximumLength);
+    // Greed Starter
+    event GreedStarterTreasuryFeesUpdated(uint16 newFee);
+    event MinimumAndMaximumProjectLengthUpdated(uint newMinimumLength, uint newMaximumLength);
+}
---
+++ contracts/abstract/HellGoverned.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "../HellGovernment.sol";
+
+
+abstract contract HellGoverned is Initializable, OwnableUpgradeable {
+    HellGovernment internal _hellGovernmentContract;
+
+    function _setHellGovernmentContract(address hellGovernmentAddress) public onlyOwner {
+        _hellGovernmentContract = HellGovernment(hellGovernmentAddress);
+        emit HellGovernmentContractUpdated(hellGovernmentAddress);
+    }
+
+    event HellGovernmentContractUpdated(address newHellGovernmentAddress);
+}
