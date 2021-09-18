8be1493dc37f3e2f4ed59dfe10eb9965835f5e83
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-18 13:19:45 -0300

    Update deployGreedStarterIndexer.ts

--- scripts/deployments/deployGreedStarterIndexer.ts
+++ scripts/deployments/deployGreedStarterIndexer.ts
@@ -4,20 +4,24 @@ import {Contract} from "ethers";
 import {ContractUtils} from "../../utils/contract-utils";
 import greedStarterIndexerSol from "../../artifacts/contracts/GreedStarterIndexer.sol/GreedStarterIndexer.json";
 import {EtherUtils} from "../../utils/ether-utils";
+import {defaultDeploymentOptions} from "../../models/deployment-options";
 
-export async function deployGreedStarterIndexer(greedStarterContractAddress: string, printLogs: boolean = true, initializeImplementation: boolean = true): Promise<Contract> {
+export async function deployGreedStarterIndexer(hellGovernmentAddress: string, greedStarterContractAddress: string, deploymentOptions = defaultDeploymentOptions): Promise<Contract> {
     const greedStarterIndexerContractProxy = await upgrades.deployProxy(
-        await ethers.getContractFactory("GreedStarterIndexer"), [greedStarterContractAddress], {kind: 'uups'});
-    if (printLogs) {
+        await ethers.getContractFactory("GreedStarterIndexer"), [
+            hellGovernmentAddress,
+            greedStarterContractAddress
+        ], {kind: 'uups'});
+    if (deploymentOptions.printLogs) {
         await Console.contractDeploymentInformation("GreedStarterIndexer", greedStarterIndexerContractProxy);
     }
 
-    if (initializeImplementation) {
+    if (deploymentOptions.initializeImplementation) {
         // Initialize Implementation with gibberish values, so that the contract is left in an unusable state.
         // https://forum.openzeppelin.com/t/security-advisory-initialize-uups-implementation-contracts/15301
         await ContractUtils.initializeProxyImplementation(greedStarterIndexerSol, greedStarterIndexerContractProxy, [
             EtherUtils.zeroAddress(),
-        ], printLogs);
+        ], deploymentOptions.printLogs);
     }
 
     return greedStarterIndexerContractProxy;
