b48e66f8b66744bda0c1ae9587998c139448856e
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-09-18 10:32:11 -0300

    Update tests.ts

--- test/tests.ts
+++ test/tests.ts
@@ -4,8 +4,10 @@ import {hellTests} from "./hell/@hellTests";
 import {auctionHouseIndexerTests} from "./auctionHouseIndexer/@auctionHouseIndexerTests";
 import {greedStarterIndexerTests} from "./greedStarterIndexer/@greedStarterIndexerTests";
 import {uupsProxiesImplementations} from "./_general/uupsProxiesImplementations";
+import {hellGovernmentTests} from "./hellGovernment/@hellGovernmentTests";
 
 describe('[Hell]', hellTests);
+describe('[Hell Government]', hellGovernmentTests);
 describe('[Auction House]', auctionHouseTests);
 describe('[Auction House Indexer]', auctionHouseIndexerTests);
 describe('[Greed Starter]', greedStarterTests);
