f0ac39b8e40b8aa9e1f2c9000950ac51204b8e25
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-09 14:41:15 -0300

    Refactor: update solidity version for IPancakeRouter

--- contracts/external/pancakeSwap/interfaces/IPancakeRouter01.sol
+++ contracts/external/pancakeSwap/interfaces/IPancakeRouter01.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.2;
+pragma solidity ^0.8.7;
 
 interface IPancakeRouter01 {
     function factory() external pure returns (address);
--- contracts/external/pancakeSwap/interfaces/IPancakeRouter02.sol
+++ contracts/external/pancakeSwap/interfaces/IPancakeRouter02.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.2;
+pragma solidity ^0.8.7;
 
 import './IPancakeRouter01.sol';
 
