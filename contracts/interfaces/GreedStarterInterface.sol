bbd8572cb6b965fcf5b6b7a9e7ef2632b54d9ab1
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-09 16:20:52 -0300

    feat: GreedStarterInterface

---
+++ contracts/interfaces/GreedStarterInterface.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// HellCash
+// https://hell.cash
+//////////////////////////////////////////
+
+pragma solidity ^0.8.7;
+
+interface GreedStarterInterface {
+    function createProject(
+        address payable tokenAddress,
+        address payable paidWith,
+        uint totalTokens,
+        uint startingBlock,
+        uint endsAtBlock,
+        uint pricePerToken,
+        uint minimumPurchase,
+        uint maximumPurchase
+    ) external;
+    function invest(uint projectId, uint amountToBuy) external;
+    function claimFunds(uint projectId) external;
+}
