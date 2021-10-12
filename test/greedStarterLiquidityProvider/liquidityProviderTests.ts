e9632291904edddbeed979d347ac1b0e236a0c2f
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-12 19:13:10 -0300

    test (UniswapV2LiquidityProvider): Should create a new project

--- test/greedStarterLiquidityProvider/liquidityProviderTests.ts
+++ test/greedStarterLiquidityProvider/liquidityProviderTests.ts
@@ -1,11 +1,17 @@
 import {greedStarterTestingEnvironment} from "../greedStarter/@greedStarterTestingEnvironment";
 import {deployUniswapV2LiquidityProvider} from "../../scripts/deployments/deployUniswapV2LiquidityProvider";
-import {Contract} from "ethers";
+import {BigNumber, Contract} from "ethers";
 import {defaultDeploymentOptions} from "../../models/deploymentOptions";
+import {Project} from "../../models/project";
+import {EtherUtils} from "../../utils/etherUtils";
+import {parseEther} from "ethers/lib/utils";
+import {ethers} from "hardhat";
+import {expect} from "chai";
 
 export function liquidityProviderTests() {
     let environment: greedStarterTestingEnvironment = new greedStarterTestingEnvironment();
     let uniswapV2LiquidityProvider: Contract;
+
     before(async() => {
         await environment.initialize();
         // We'll test with UniswapV2 from Ethereum
@@ -17,4 +23,27 @@ export function liquidityProviderTests() {
         , defaultDeploymentOptions);
     });
 
+    let project: Project;
+    it('Should create a new project', async() => {
+        // Increase Allowance
+        await environment.hellContract.approve(uniswapV2LiquidityProvider.address, parseEther("200"));
+        const currentBlock = await ethers.provider.getBlockNumber();
+        const totalProjects: BigNumber = await environment.greedStarterContract._totalProjects();
+        await uniswapV2LiquidityProvider.createProject(
+            environment.hellContract.address, // Token address
+            EtherUtils.zeroAddress(), // Address of paying currency
+            parseEther("100"), // Total Tokens
+            currentBlock + 25, // Starting block
+            environment.minimumProjectLength.mul(2).add(currentBlock), // Ending block
+            parseEther("0.001"), // Price per token
+            parseEther("1"), // Minimum purchase
+            parseEther("10"), // Maximum Purchase
+            parseEther("100") // Initial Liquidity
+        );
+        const afterTotalProjects: BigNumber = await environment.greedStarterContract._totalProjects();
+        // Verify if the total number of projects Increased
+        expect(afterTotalProjects).to.be.equal(totalProjects.add(1));
+        // Retrieve the project details
+        project = (await environment.greedStarterContract.getProjects([afterTotalProjects]))[0];
+    });
 }
