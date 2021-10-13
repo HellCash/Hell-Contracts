8e6323503155faea207dc2df730dd80ee8cd6a69
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-12 21:03:50 -0300

    refactor: HellInterface as IHell

--- contracts/abstract/IHell.sol
+++ contracts/abstract/IHell.sol
@@ -6,6 +6,6 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-abstract contract HellInterface is IERC20Upgradeable {
+abstract contract IHell is IERC20Upgradeable {
     function mintVaultRewards(uint256 amount) external virtual {}
 }
