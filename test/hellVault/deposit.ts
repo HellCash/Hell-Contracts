a2e7f6096ec5cb26b2fd30f306466b7492c46e72
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-13 16:59:04 -0300

    wip (Hell Vault deposit): await for expects and Increase allowance on deposit

--- test/hellVault/deposit.ts
+++ test/hellVault/deposit.ts
@@ -9,29 +9,30 @@ export function deposit() {
     });
 
     it('Should fail if deposit is less than 1e12', async() => {
-        expect(environment.hellVaultContract
+        await expect(environment.hellVaultContract
             .deposit(parseUnits("1", 11)) // REVERT <-- we are providing 1e11
         ).to.be.revertedWith("D1");
     });
 
     it("Should fail if user doesn't have enough balance", async() => {
-        expect(environment.hellVaultContract
+        await expect(environment.hellVaultContract
             .connect(environment.guest1Signer) // REVERT <-- guest1 has no balance
             .deposit(parseEther("1"))).to.be.revertedWith("DA2");
     });
 
     it("Should fail if user doesn't have enough allowance", async() => {
-        expect(environment.hellVaultContract
+        await expect(environment.hellVaultContract
             .deposit(parseEther("1")) // REVERT <-- the masterSigner has no allowance.
         ).to.be.revertedWith("DA3");
     });
 
     it('Should perform a successful deposit with 1e12', async() => {
-        const depositAmount = parseUnits("1", 12);
-        expect(environment.hellVaultContract
-            .deposit(depositAmount))
+        const amount = parseUnits("1", 12);
+        await environment.hellContract.approve(environment.hellVaultContract.address, amount);
+        await expect(environment.hellVaultContract
+            .deposit(amount))
             .to.emit(environment.hellVaultContract, "Deposit")
-            .withArgs(environment.masterSigner.address, depositAmount);
+            .withArgs(environment.masterSigner.address, amount);
         // TODO: Verify that the userInfo was updated successfully
     });
 }
