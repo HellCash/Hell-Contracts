5db81320b22a8a8b79865cb47655bf8c9f6f9792
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-14 20:54:29 -0300

    wip (HellVault): Updated rewardPerBlock to work with deposits of 1e12

--- scripts/deployments/deployHellVault.ts
+++ scripts/deployments/deployHellVault.ts
@@ -15,17 +15,17 @@ export async function deployHellVault(hellContractAddress: string, hellGovernmen
         console.log("Hell Vault Contract: Setting up dividend periods");
     }
     await hellVaultContractProxy._setDividendPeriods([
-        { from: 0,   to: 199, rewardPerBlock:  6335616438},
-        { from: 200, to: 250, rewardPerBlock:  4892774698},
-        { from: 251, to: 300, rewardPerBlock:  3521682070},
-        { from: 301, to: 350, rewardPerBlock:  2597036494},
-        { from: 351, to: 400, rewardPerBlock:  1919118946},
-        { from: 401, to: 450, rewardPerBlock:  1393168002},
-        { from: 451, to: 500, rewardPerBlock:   993303416},
-        { from: 501, to: 550, rewardPerBlock:   662580933},
-        { from: 551, to: 600, rewardPerBlock:   359778564},
-        { from: 601, to: 650, rewardPerBlock:   218797564},
-        { from: 651, to: 666, rewardPerBlock:   116872144},
+        { from: 0,   to: 199, rewardPerBlock:   633562},
+        { from: 200, to: 250, rewardPerBlock:   489277},
+        { from: 251, to: 300, rewardPerBlock:   352168},
+        { from: 301, to: 350, rewardPerBlock:   259704},
+        { from: 351, to: 400, rewardPerBlock:   191912},
+        { from: 401, to: 450, rewardPerBlock:   139317},
+        { from: 451, to: 500, rewardPerBlock:    99330},
+        { from: 501, to: 550, rewardPerBlock:    66258},
+        { from: 551, to: 600, rewardPerBlock:    35978},
+        { from: 601, to: 650, rewardPerBlock:    21880},
+        { from: 651, to: 666, rewardPerBlock:    11687},
     ]);
     return hellVaultContractProxy;
 }
