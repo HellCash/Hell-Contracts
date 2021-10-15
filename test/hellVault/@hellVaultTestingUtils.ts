5c3c366b55a76ad3e5f00ab0c8578258c439d4e2
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-15 00:23:34 -0300

    wip (HellVaultTestingUtils): split logs on different functions

--- test/hellVault/@hellVaultTestingUtils.ts
+++ test/hellVault/@hellVaultTestingUtils.ts
@@ -4,21 +4,22 @@ import {HellVaultUserInfo} from "../../models/hellVaultUserInfo";
 import {formatEther} from "ethers/lib/utils";
 
 export class HellVaultTestingUtils {
-    static async logVaultAndUserInfo(environment: HellVaultTestingEnvironment, signer: any | null = null) {
+    static async logVaultInfo(environment: HellVaultTestingEnvironment) {
+        console.log(`\t\t[Vault Data]`);
+        console.log(`\t\t\t_lastDividendBlock: ${await environment.hellVaultContract._lastDividendBlock()}`);
+        const dividendPeriodIndex = await environment.hellVaultContract.getDividendPeriodIndex();
+        console.log(`\t\t\t_dividendPeriodIndex: ${dividendPeriodIndex[1]} (Status ${dividendPeriodIndex[0]})`);
+        console.log(`\t\t\t_distributedDividends: ${await environment.hellVaultContract.getDistributedDividends()}`);
+        const totalAmountDeposited = await environment.hellVaultContract._totalAmountDeposited();
+        console.log(`\t\t\t_totalAmountDeposited: ${formatEther(totalAmountDeposited)} (${totalAmountDeposited} wei)`);
+    }
+
+    static async logUserData(environment: HellVaultTestingEnvironment, signer: any | null = null) {
         if (signer == null) {
             signer = environment.masterSigner;
         }
-        const blockNumber = await ethers.provider.getBlockNumber();
-        console.log(`\tBlock number: ${blockNumber}`);
-        const userData: HellVaultUserInfo = await environment.hellVaultContract.getUserInfo(signer.address);
-        console.log(`\t\t[Vault Data]`);
-        console.log(`\t\t\t_lastDividendBlock: ${await environment.hellVaultContract._lastDividendBlock()}`);
-        const dividendPeriodIndex = await environment.hellVaultContract._dividendPeriodIndex();
-        console.log(`\t\t\t_dividendPeriodIndex: ${dividendPeriodIndex[1]} (Status ${dividendPeriodIndex[0]})`);
-        console.log(`\t\t\t_distributedDividends: ${await environment.hellVaultContract.distributedDividends()}`);
-        const totalAmountDeposited = await environment.hellVaultContract._totalAmountDeposited();
-        console.log(`\t\t\t_totalAmountDeposited: ${formatEther(totalAmountDeposited)} (${totalAmountDeposited} wei)`);
         console.log(`\t\t[User Data]`);
+        const userData: HellVaultUserInfo = await environment.hellVaultContract.getUserInfo(signer.address);
         console.log(`\t\t\tDeposited: ${formatEther(userData.hellDeposited)} (${userData.hellDeposited} wei)`);
         console.log(`\t\t\tLastDividend: ${userData.lastDividendBlock}`);
         console.log(`\t\t\tDistributedDividends: ${userData.distributedDividendsSinceLastPayment}`);
@@ -27,5 +28,11 @@ export class HellVaultTestingUtils {
         console.log(`\t\t\tWithdrawFee: ${formatEther(userData.hellRewardWithdrawFee)} (${userData.hellRewardWithdrawFee} wei)`);
         const rewardAfterFee = userData.hellRewarded.sub(userData.hellRewardWithdrawFee);
         console.log(`\t\t\tRewardAfterFee: ${formatEther(rewardAfterFee)} (${rewardAfterFee} wei)`);
+    }
+
+    static async logVaultAndUserInfo(environment: HellVaultTestingEnvironment, signer: any | null = null) {
+        console.log(`\tBlock number: ${await ethers.provider.getBlockNumber()}`);
+        await this.logVaultInfo(environment);
+        await this.logUserData(environment, signer);
     }
 }
