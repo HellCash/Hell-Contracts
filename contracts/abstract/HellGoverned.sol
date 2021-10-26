a865591a7c0ec367228ceb31cdb50f3387d08cd7
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-26 17:38:07 -0300

    refactor (HellGoverned): removed unrequired Initializable contract

--- contracts/abstract/HellGoverned.sol
+++ contracts/abstract/HellGoverned.sol
@@ -5,12 +5,10 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "../HellGovernment.sol";
 
-
-abstract contract HellGoverned is Initializable, OwnableUpgradeable {
+abstract contract HellGoverned is OwnableUpgradeable {
     HellGovernment internal _hellGovernmentContract;
 
     function _setHellGovernmentContract(address hellGovernmentAddress) public onlyOwner {
