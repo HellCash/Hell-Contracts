d0db802b44d6ff5a40031f6dae0001d0608dadd9
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-10-28 10:52:33 -0300

    test (HellVault): _minimumDeposit

--- test/hellVault/@hellVaultTests.ts
+++ test/hellVault/@hellVaultTests.ts
@@ -4,11 +4,13 @@ import {initialize} from "./initialize";
 import {withdrawSingleUser} from "./withdrawSingleUser";
 import {upgradeTo} from "./upgradeTo";
 import {upgradeToAndCall} from "./upgradeToAndCall";
+import {_minimumDeposit} from "./_minimumDeposit";
 
 export function hellVaultTests() {
     describe('function initialize', initialize);
     describe('function upgradeTo', upgradeTo);
     describe('function upgradeToAndCall', upgradeToAndCall);
+    describe('function _minimumDeposit', _minimumDeposit);
     describe('function deposit', deposit);
     describe('function withdraw', withdraw);
     describe('function withdraw (Single user)', withdrawSingleUser);
---
+++ test/hellVault/_minimumDeposit.ts
@@ -0,0 +1,17 @@
+import {HellVaultTestingEnvironment} from "./@hellVaultTestingEnvironment";
+import {expect} from "chai";
+import {formatUnits, parseUnits} from "ethers/lib/utils";
+import {BigNumber} from "ethers";
+
+export function _minimumDeposit() {
+    let environment: HellVaultTestingEnvironment = new HellVaultTestingEnvironment();
+
+    before(async () => {
+        await environment.initialize();
+    });
+
+    it('Should return 1e12', async() => {
+        const minimumDeposit: BigNumber = await environment.hellVaultContract._minimumDeposit();
+        expect(minimumDeposit).to.be.equal(parseUnits("1", 12));
+    });
+}
