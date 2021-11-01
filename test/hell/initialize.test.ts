25a55eb028a1eb34e171da10b9f515fe29eca84d
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-11-01 17:14:00 -0300

    fix (Hell initialize test): Missing supply param

--- test/hell/initialize.test.ts
+++ test/hell/initialize.test.ts
@@ -8,7 +8,7 @@ export function initialize() {
     });
 
     it('should already be initialized', async() => {
-        await expect(environment.hellContract.connect(environment.guest1Signer).initialize('Hell', 'HELL')).to.be
+        await expect(environment.hellContract.connect(environment.guest1Signer).initialize('Hell', 'HELL', 566)).to.be
             .revertedWith("Initializable: contract is already initialized");
     });
 }
