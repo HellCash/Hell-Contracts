a734ed12eafa36ef19ccff4510de7002a529fef1
Joaquín Céspedes <joaquin@jcespedes.dev>
2021-11-04 18:18:42 -0300

    chore (HellVaultBonusTestingEnvironment): function logBonusAndUserInfo

--- test/hellVaultBonus/@hellVaultBonusTestingEnvironment.ts
+++ test/hellVaultBonus/@hellVaultBonusTestingEnvironment.ts
@@ -1,10 +1,12 @@
 import {HellVaultTestingEnvironment} from "../hellVault/@hellVaultTestingEnvironment";
-import {parseEther} from "ethers/lib/utils";
+import {formatEther, parseEther} from "ethers/lib/utils";
 import {deployRandom} from "../../scripts/deployments/deployRandom";
 import {testingEnvironmentDeploymentOptions} from "../../models/deploymentOptions";
 import {HellVaultBonusInfo} from "../../models/hellVaultBonusInfo";
 import {Random} from "../../utils/random";
 import {BigNumber} from "ethers";
+import {ethers} from "hardhat";
+import erc20Sol from "../../artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json";
 
 export class HellVaultBonusTestingEnvironment extends HellVaultTestingEnvironment {
     async initialize(): Promise<void> {
@@ -34,5 +36,25 @@ export class HellVaultBonusTestingEnvironment extends HellVaultTestingEnvironmen
         }
         // Return HellVaultBonuses after their addition
         return await this.hellVaultBonusContract.getCurrentBonuses();
+    }
+
+    async logBonusAndUserInfo() {
+        const bonusesInfo: HellVaultBonusInfo[] = await this.hellVaultBonusContract.getCurrentBonuses();
+        console.log(`\tBlock number: ${await ethers.provider.getBlockNumber()}`);
+        for (let bonusInfo of bonusesInfo) {
+            const bonusContract = await ethers.getContractAt(erc20Sol.abi, bonusInfo.tokenAddress);
+            const userBalance = await bonusContract.balanceOf(this.guest1Signer.address);
+            console.log(`\n\t\t[Project Info]`);
+            console.log(`\t\tToken: ${bonusContract.address}`);
+            console.log(`\t\tReward Per block: ${formatEther(bonusInfo.rewardPerBlock)} (${bonusInfo.rewardPerBlock} wei)`);
+            console.log(`\t\tTotal Amount: ${formatEther(bonusInfo.totalAmount)} (${bonusInfo.totalAmount} wei)`);
+            console.log(`\t\tAmount Available: ${formatEther(bonusInfo.amountAvailable)} (${bonusInfo.amountAvailable} wei)`);
+            console.log(`\t\tStarting block: ${bonusInfo.startingBlock}`);
+            console.log(`\t\tEnded at block: ${bonusInfo.endedAtBlock}`);
+            console.log(`\t\t[User Info]`);
+            console.log(`\t\tBalance: ${formatEther(userBalance)} (${userBalance} wei)`);
+            console.log(`\t\tBlocks Earned: ${bonusInfo.blocksEarned}`);
+            console.log(`\t\tUnrealized rewards: ${bonusInfo.userUnrealizedRewards}`);
+        }
     }
 }
